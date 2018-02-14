1ee8724ec17272026ffd679bfd53298556fb90c7
Kaan Sentürk <skaan@student.ethz.ch>
2018-02-14 15:33:30 +0100

    some sort of token implementation

--- voting-system-react/app/votingsystem.js
+++ voting-system-react/app/votingsystem.js
@@ -31,6 +31,8 @@ const abi = [
 	},
 	{
 		"constant":true,
-		"inputs":[{"name":"","type":"uint256"}],"name":"votings","outputs":[{"name":"title","type":"string"},{"name":"description","type":"string"},{"name":"minimumContribution","type":"uint256"},{"name":"complete","type":"bool"},{"name":"approvalCount","type":"uint256"},{"name":"rejectionCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"index","type":"uint256"},{"name":"value","type":"bool"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+		"inputs":[{"name":"","type":"uint256"}],
+		"name":"votings",
+		"outputs":[{"name":"title","type":"string"},{"name":"description","type":"string"},{"name":"minimumContribution","type":"uint256"},{"name":"complete","type":"bool"},{"name":"approvalCount","type":"uint256"},{"name":"rejectionCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"index","type":"uint256"},{"name":"value","type":"bool"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address);
--- voting-system/contracts/VotingSystem.sol
+++ voting-system/contracts/VotingSystem.sol
@@ -4,16 +4,16 @@ contract VotingSystem {
     struct Voting {
         string title;
         string description;
-        uint minimumContribution;
         bool complete;
         uint approvalCount;
         uint rejectionCount;
-        mapping(address => bool) alreadyVoted;
+        mapping(address => uint) token;
         mapping(address => bool) voters;
     }
     
     Voting[] public votings;
     address manager;
+    mapping(address => bool) voters;
     
     modifier restricted() {
         require(msg.sender == manager);
@@ -24,11 +24,14 @@ contract VotingSystem {
         manager = msg.sender;
     }
     
-    function createVoting(string title, string description, uint minimum) public restricted {
+    function getVotingsCount() public view returns (uint) {
+        return votings.length;
+    }
+    
+    function createVoting(string title, string description) public restricted {
         Voting memory newVoting = Voting({
             title: title, 
             description: description,
-            minimumContribution: minimum,
             complete: false,
             approvalCount: 0, 
             rejectionCount: 0
@@ -37,26 +40,35 @@ contract VotingSystem {
         votings.push(newVoting);
     }
     
-    function getVotingTitle(uint index) public view returns (string){
+    function getVotingData(uint index) public view returns (string){
         return votings[index].title;
     }
     
+    function enableVoting(address voter) public restricted{
+        voters[voter] = true;
+    }
+    
     function enterVoting(uint index) public payable {
+        require(voters[msg.sender]);
         Voting storage voting = votings[index];
-        require(msg.value >= voting.minimumContribution);
+        require(!voting.voters[msg.sender]);
+        voting.token[msg.sender] = 1;
         voting.voters[msg.sender] = true;
     }
     
     function vote(uint index, bool value) public {
         Voting storage voting = votings[index];
         require(voting.voters[msg.sender]);
-        require(!voting.alreadyVoted[msg.sender]);
+        require(voting.token[msg.sender] >= 1);
         
-        voting.alreadyVoted[msg.sender] = true;
+        voting.token[msg.sender] -= 1;
         if (value) {
             voting.approvalCount++;
         } else {
             voting.rejectionCount++;
         }
     }
+    
+    
+    
 }
