80043d226025d4285215acdbf4c5d00622a238de
Kaan Sentürk <skaan@student.ethz.ch>
2018-02-15 15:33:31 +0100

    separate contracts

---
+++ voting-system/contracts/ProposalSystem.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.4.18;
+
+import "./VotingSystem.sol";
+import "./TokenERC20.sol";
+
+contract ProposalSystem {
+    
+    struct Proposal {
+        string title;
+        string description;
+        address manager;
+        uint needApprovals;
+        bool complete;
+        uint numberOfApprovals;
+        mapping(address => bool) alreadyJoined;
+        uint endTime;
+    } 
+    
+    VotingSystem public vSytem;
+    Proposal[] public proposals;
+    
+    function ProposalSystem(address _vsystem) public {
+        vSytem = VotingSystem(_vsystem);
+    }
+    
+    function createProposal(string title, string description, uint minimum, uint duration) public {
+        
+        //overflow check
+        require(now <= now + duration);
+
+        // create proposal instance
+        Proposal memory newProposal = Proposal({
+            title: title,
+            description: description,
+            manager: msg.sender,
+            complete: false,
+            needApprovals: minimum,
+            numberOfApprovals: 0,
+            endTime: now + duration
+        });
+        
+        proposals.push(newProposal);
+    }
+    
+    function createVotingForProposal(uint _index, uint duration, bool _transferVoteAllowed) public payable {
+        Proposal storage proposal = proposals[_index];
+        require(now <= proposal.endTime);
+        //overflow check
+        require(now <= now + duration);
+        
+        require(proposal.manager == msg.sender);
+        require(proposal.numberOfApprovals >= proposal.needApprovals);
+        
+        vSytem.createVoting(proposal.title, proposal.description, duration, _transferVoteAllowed);
+    }
+
+    function getProposalCount() public view returns (uint) {
+        return proposals.length;
+    }
+
+    function supportProposal(uint index) public {
+        Proposal storage proposal = proposals[index];
+        require(now <= proposal.endTime);
+        
+        require(!proposal.alreadyJoined[msg.sender]);
+        
+        proposal.alreadyJoined[msg.sender] = true;
+        proposal.numberOfApprovals++;
+    }
+    
+    function checkProposalStatus (uint index) public view returns (bool) {
+        Proposal storage proposal = proposals[index];
+        require(now <= proposal.endTime);
+
+        bool result = proposal.numberOfApprovals > proposal.needApprovals;
+        return result;
+    }
+}
--- voting-system/contracts/VotingSystem.sol
+++ voting-system/contracts/VotingSystem.sol
@@ -19,27 +19,14 @@ contract VotingSystem {
         mapping(address => bool) voters;
         mapping(address => VoteCounter) votes;
     }
-    
-    struct Proposal {
-        string title;
-        string description;
-        address manager;
-        uint needApprovals;
-        bool complete;
-        uint numberOfApprovals;
-        mapping(address => bool) alreadyJoined;
-        uint endTime;
-    } 
 
     struct VoteCounter {
         uint yesVotes;
         uint noVotes;
     }
-
     
     // system storage
     Voting[] public votings;
-    Proposal[] public proposals;
     address public manager;
     uint public votersCount;
     mapping(address => bool) public voters;
@@ -92,41 +79,12 @@ contract VotingSystem {
         
         votings.push(newVoting);
     }
-
-    function finalizeVoting(uint index) public restricted returns (uint, uint, uint) {
-        Voting storage voting = votings[index];
-        require(now > voting.endTime);
-        voting.complete = true;
-
-        return(voting.maxVoterCount, voting.approvalCount, voting.rejectionCount);
-    }
     
-    function enableVoter(address voter) public restricted {
+    function enableVoting(address voter) public restricted {
         require(!voters[voter]);
         votersCount++;
         voters[voter] = true;
     }
-
-    // we chose not to include this as it would give the owner power to manipulate
-    // a vote
-    /*function disableVoter(address voter, uint index) {
-        require(voters[voter]);
-        votersCount--;
-        voters[voter] = false;
-
-        Voting storage voting = votings[index];
-        require(now <= voting.endTime);
-        uint yesCount = voting.votes[voter].yesVotes;
-        uint noCount = voting.votes[voter].noVotes;
-
-        // reset voters vote counts
-        voting.votes[voter].yesVotes = 0;
-        voting.votes[voter].noVotes = 0;
-
-        // revert a voters votes
-        voting.approvalCount -= yesCount;
-        voting.rejectionCount -= noCount;
-    }*/
     
     function enterVoting(uint index) public payable {
         require(voters[msg.sender]);
@@ -237,75 +195,4 @@ contract VotingSystem {
         voting.token.approveWithSender(msg.sender, _to, 1);
     } 
     
-    // =====================================
-    // Proposal functions
-    // =====================================
-    
-    // duration is time in seconds for which proposal is open
-    function createProposal(string title, string description, uint minimum, uint duration) public {
-        //overflow check
-        require(now <= now + duration);
-
-        Proposal memory newProposal = Proposal({
-            title: title,
-            description: description,
-            manager: msg.sender,
-            complete: false,
-            needApprovals: minimum,
-            numberOfApprovals: 0,
-            endTime: now + duration
-        });
-        
-        proposals.push(newProposal);
-    }
-    
-    // duration is time in seconds for which voting is open
-    // transferVoteAllowed specifies if votes can be transfered to another person
-    function createVotingForProposal(uint _index, uint duration, bool _transferVoteAllowed) public payable {
-        Proposal storage proposal = proposals[_index];
-        require(now <= proposal.endTime);
-        //overflow check
-        require(now <= now + duration);
-        
-        require(proposal.manager == msg.sender);
-        require(proposal.numberOfApprovals >= proposal.needApprovals);
-        
-        TokenERC20 newToken = new TokenERC20(votersCount, "VoteCoin", "VTC");
-        Voting memory newVoting = Voting({
-            title: proposal.title,
-            description: proposal.description,
-            approvalCount: 0,
-            rejectionCount: 0,
-            maxVoterCount: votersCount,
-            balance: msg.value,
-            token: newToken,
-            endTime: now + duration,
-            complete: false,
-            transferVoteAllowed: _transferVoteAllowed
-        });
-        
-        votings.push(newVoting);
-    }
-
-    function getProposalCount() public view returns (uint) {
-        return proposals.length;
-    }
-
-    function supportProposal(uint index) public {
-        Proposal storage proposal = proposals[index];
-        require(now <= proposal.endTime);
-        
-        require(!proposal.alreadyJoined[msg.sender]);
-        
-        proposal.alreadyJoined[msg.sender] = true;
-        proposal.numberOfApprovals++;
-    }
-    
-    function checkProposalStatus (uint index) public view returns (bool) {
-        Proposal storage proposal = proposals[index];
-        require(now <= proposal.endTime);
-
-        bool result = proposal.numberOfApprovals > proposal.needApprovals;
-        return result;
-    }
 }
